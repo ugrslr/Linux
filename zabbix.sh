f7a9c7a39ea10281f2069147631ab3579278ea3e
ugrslr <ugrslr@gmail.com>
2023-11-24 16:12:05 +0300

    update

--- zabbix.sh
+++ zabbix.sh
@@ -1,56 +1,39 @@
+#! /bin/bash
+
 ##---------Zabbix Paketi İndirme----------##
 
-wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
+wget https://github.com/ugrslr/Linux/raw/main/zabbix-6-4-1-ubuntu-22-04-all.deb
 
-dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
+dpkg -i zabbix-6-4-1-ubuntu-22-04-all.deb
 
 apt update
 
 ##---------Postgre Yükleme--------##
 
-sudo apt update
+apt install postgresql postgresql-contrib -y
 
-sudo apt install postgresql postgresql-contrib -y
-
-sudo systemctl start postgresql.service
-
-#sudo -i -u postgres
-
-#Psql
-
-#\l 
+systemctl start postgresql.service
 
 ##-------Install Zabbix server, frontend, agent---------##
 
 apt install zabbix-server-pgsql zabbix-frontend-php php8.1-pgsql zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
 
-chmod 777 /home/$user
-
-#(Kullanıcı dizinine Full erişim yetkisi vermek gerekiyor. "satu" yazan yere, sizin kurulumda verdiğiniz user'ı vermeniz  gerekiyor. )#
+chmod 777 $HOME
 
 #-----------Create initial database----------------#
 
-sudo -u postgres createuser --pwprompt zabbix
+sudo -u postgres createuser zabbix -P
+
+echo "DB kullanıcısı için vermiş olduğunuz şifreyi bir daha girin:"
+
+read dbpswd
 
 sudo -u postgres createdb -O zabbix zabbix
 
 zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
 
-echo 
-
-#nano /etc/zabbix/zabbix_server.conf
-
-# (Zabbix konfigürasyon dosyasında düzenleme yapmak için nano editörü ile erişiyoruz)
-
-DBPassword=password
-
-#(Burada, database kullanıcısı oluştururken verdiğimiz şifreyi giriyoruz.)#
+echo "DBpassword=$dbpswd" >> /etc/zabbix/zabbix_server.conf
 
 systemctl restart zabbix-server zabbix-agent apache2
 
 systemctl enable zabbix-server zabbix-agent apache2
-
-
-
-Zabbix Default User: Admin
-zabbix Default PW: zabbix
